5bfb40c669768dd0f944e9600c6c0f25100b0764
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-05-17 15:42:48 +0100

    mysql connection

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const express = require(express);
-const app = express;
-
-app.get("/", (req, res) => {
-    res.send("<h1>Home page</h1>")
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const db = mysql.createConnection({
+    host:'localhost',
+    user: 'root', 
+    password:'',
+    database:'ti-login'
 });
+
+db.connect((err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("MYSQL Connected");
+    }
+})
+
+app.get("/",(req,res)=>{
+    res.send("I love Basketball")
+})
 app.listen(3001, () => {
     console.log("Server started on port 3001")
 })
