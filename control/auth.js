415369ad6afc5b3e99f19bfe6bcc952c5e73656c
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-05-18 14:52:13 +0100

    Debbuging CSS routes and adding check Icon on account

--- control/auth.js
+++ control/auth.js
@@ -1,5 +1,6 @@
 exports.connexion = (req, res) => {
     // Logs all the data w'eve gotten from the form
+    console.log("Requête reçue !");
     console.log(req.body);
     res.send("Form Sublitted");
 }
