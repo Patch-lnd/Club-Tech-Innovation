0218ee8d3c5e31b9b15de0366407e9fa48a45753
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-12-30 00:20:17 +0100

    Adding login expired  OTP codes auto delete to the function

--- middleware/pendingUserCleanup.js
+++ middleware/pendingUserCleanup.js
@@ -1,5 +1,6 @@
 // This file is responsible for cleaning up pending users who have not verified their email within a certain timeframe.
-// Fron the pending_user table at a fixed interval (e.g., every hour).
+// From the pending_user table at a fixed interval (e.g., every hour).
+// FROM otp_codes (Login OTP)
 
 // Why ? 
 // - Email verification tokens expires after 15 minutes 
@@ -10,7 +11,7 @@ module.exports = function startPendingUserCleanup(db) {
     const CLEANUP_INTERVAL = 20*60*1000; // For Now, 20 mins  in milliseconds
 
     setInterval(()=>{
-        console.log("⏳ Running DB cleanup for expired pending users...");
+        console.log("⏳ Running DB cleanup for expired Auth user's token...");
 
        // SQL query to delete pending users whose email verification token has expired
         const cleanupQuery =`DELETE FROM pending_users WHERE token_expires_at < NOW()`;
@@ -27,5 +28,22 @@ module.exports = function startPendingUserCleanup(db) {
                 console.log("✅ Cleanup executed: No expired pending users to clean up at this time.");
             }
         })
+
+        // CLEANUP OTP CODES
+        // Delete OTPs that are expired OR already used
+
+        const otpCleanupQuery = `DELETE FROM otp_codes WHERE expires_at < NOW()`;
+        db.query(otpCleanupQuery, (err, result)=>{
+            if(err){
+                console.log("❌ Error cleaning otp_codes:", err);
+                return;
+            }
+            if(result.affectedRows > 0){
+                console.log(  `✅ Cleanup Success: ${result.affectedRows} expired/used OTP codes removed.`)
+            }else{
+                console.log("✅ Cleanup executed: No expired or used OTP codes to clean up at this time.")
+            }
+        })
+
     }, CLEANUP_INTERVAL)
 }
