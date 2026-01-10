4d26907e6d8ba23c8605037a716e7bccee78b7a5
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2026-01-11 00:04:20 +0100

    Building up the permissions middleware and thus making our dashboard dynamic to autorities

--- routes/pages.js
+++ routes/pages.js
@@ -2,6 +2,8 @@ const express = require("express");
 const authRoutes = require("./auth")
 const {protect} = require("../middleware/authMiddleware");
 const {redirectIfAuthenticated} = require("../middleware/redirectIfAuthenticated"); // Importing the middleware to redirect users to dashboard if the are connected and try to go to login or register
+const dashboardController = require("../controller/dashboardController");
+const permissionsMiddleware = require("../middleware/attachPermissions")
 
 const router = express.Router();
 
@@ -34,10 +36,15 @@ router.get('/login', redirectIfAuthenticated, (req, res) => {
   res.render('login', { message: null, success: null }); // ensure message is always defined
 });
 // Route to get to the dashboard after login 
-router.get('/dashboard', protect, (req, res)=>{
+router.get('/dashboard', protect, permissionsMiddleware, dashboardController.dashboard);
+// When a useer accesses dashboard page, we first protect the route to ensure only authenticated users can access it.
+//  Then, we attach the permissions based on their role using the permissionsMiddleware.
+//  Finally, we call the dashboardController to render the dashboard view with the user data and their permissions.
+
+/* router.get('/dashboard', protect, permissionsMiddleware, dashboardController.dashboard,(req, res)=>{
     // Defining the user's attributes for my dashboard view which depends on the users' role
     res.render("dashboard", {user: req.user})
-});
+}); */
 
 router.get('/logout', (req, res)=>{
     res.clearCookie("token");
