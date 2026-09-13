52bb8513f0c4d90872388fab2520eeb7655932af
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2026-09-13 01:00:24 +0100

    Just spacing up the code, no change yet

--- routes/pages.js
+++ routes/pages.js
@@ -11,24 +11,24 @@ const router = express.Router();
 
 router.get("/",(req,res)=>{
     res.render("index");
-    // res.send("I love Basketball") RESPOND SENT INTO THE BROWNSER
 });
+
 router.get("/compte",(req,res)=>{
     res.render("compte");
-    // res.send("I love Basketball") RESPOND SENT INTO THE BROWNSER
 });
+
 router.get("/projects",(req,res)=>{
     res.render("projects");
-    // res.send("I love Basketball") RESPOND SENT INTO THE BROWNSER
 });
+
 router.get("/blog",(req,res)=>{
     res.render("blog");
-    // res.send("I love Basketball") RESPOND SENT INTO THE BROWNSER
 });
+
 router.get("/apropos",(req,res)=>{
     res.render("apropos");
-    // res.send("I love Basketball") RESPOND SENT INTO THE BROWNSER
 });
+
 // This GET route serves the signup form
 router.get('/connexion', redirectIfAuthenticated, (req, res) => {
   res.render('connexion', { message: null, success: null }); // ensure message is always defined
